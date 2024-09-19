e93afd3043645b000d8c26a5ce3a773c4192d24a
mikek182 <marcosvinicius@geekwork.com.br>
2024-09-19 20:40:00 -0300

    Criação do preloader

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Container, Title} from './styles';
+import React, { useState, useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { Container, Preloader, Logotipo, Title } from './styles';
+import Logo from './../../assets/logo.png'; // Caminho da imagem do logo
 
 export const Home: React.FunctionComponent = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula carregamento de dados (3 segundos)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
     return (
-        <Container>
-            <Title>Home page</Title>
-        </Container>
+      <Preloader>
+        <Logotipo source={Logo} />
+        <ActivityIndicator size={60} color="yellow" />
+      </Preloader>
     );
-}
+  }
+
+  return (
+    <Container>
+      <Title>Home page</Title>
+    </Container>
+  );
+};
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -8,5 +8,18 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
+    color: #f2f2f2;
+`;
 
+export const Preloader = styled.View`
+    background-color: #118B59;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Logotipo = styled.Image`
+    width: 200px;  
+    height: 200px; 
+    margin-bottom: 40px;
 `;
