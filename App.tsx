d96cbf4b1202570ec0d588336ffc9f5036ff93a5
mikek182 <marcosvinicius@geekwork.com.br>
2024-09-20 13:18:49 -0300

    Instação de fontes

--- App.tsx
+++ App.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Home } from './src/pages/home';
 import theme from './src/global/styles/theme';
+import {useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
   <ThemeProvider theme={theme}>
