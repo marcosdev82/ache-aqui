fb2d5000ec6ca4156f04986bb901c7a19f72605a
mikek182 <marcosvinicius@geekwork.com.br>
2024-09-20 12:45:39 -0300

    estilo global

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Home } from './src/pages/home';
+import theme from './src/global/styles/theme';
 
 const App: React.FunctionComponent = () => {
-  return <Home/>
+
+  return (
+  <ThemeProvider theme={theme}>
+    <Home/>
+  </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+        green100: '#87DA69',
+        green200: '#12A065',
+        green500: '#095032',
+        yellow300: '#FFB902',
+        grey200: '#EFEFEF',
+        grey100: '#f1f1f1' ,
+        white: '#ffffff',
+    },
+};
